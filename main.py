935f281556690426e752460b6c667c313245ff03
Yooby Gigandet <yooby@macbook-pro-de-yooby.home>
2025-11-25 09:07:11 +0100

    sidebar feature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import streamlit as st
+# pylint: disable=missing-module-docstring
+
 import duckdb
+import streamlit as st
 
 st.write(
     """
@@ -12,22 +14,25 @@ Application for reviewing programming languages
 # It will need to be modified !!!
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-option_languages = st.selectbox(
-    "What would you like to review?",
-    ["SQL", "Python"],
-    index=None,
-    placeholder="Select programming language",
-)
-
-if option_languages == "SQL":
-    st.selectbox(
-        "Select themes",
-        ("cross_joins", "inner_joins"),
+with st.sidebar:
+    option_languages = st.selectbox(
+        "What would you like to review?",
+        ["SQL", "Python"],
         index=None,
-        placeholder="Select the theme you want to review",
+        placeholder="Select programming language",
     )
-else:
-    st.selectbox("Select themes", (), index=None, placeholder="This is not done yet")
+
+    if option_languages == "SQL":
+        st.selectbox(
+            "Select themes",
+            ("cross_joins", "inner_joins"),
+            index=None,
+            placeholder="Select the theme you want to review",
+        )
+    else:
+        st.selectbox(
+            "Select themes", (), index=None, placeholder="This is not done yet"
+        )
 
 # query = st.text_area("Enter your query")
 #
