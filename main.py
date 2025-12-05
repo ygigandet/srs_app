502b60357ceecd64b339c9a6e927325cf683411e
Yooby Gigandet <yooby@macbook-pro-de-yooby.home>
2025-12-05 09:59:50 +0100

    pylint, black and isort for main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 import logging
 import os
-
 from datetime import date, timedelta
+
 import duckdb
 import streamlit as st
 
@@ -26,7 +26,7 @@ Application for reviewing programming languages
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-with (st.sidebar):
+with st.sidebar:
     available_theme_df = con.execute("SELECT DISTINCT theme FROM memory_state").df()
     selected_theme = st.selectbox(
         "Select theme:",
@@ -35,7 +35,9 @@ with (st.sidebar):
         placeholder="Select theme",
     )
     if selected_theme:
-        select_exercise_query = f"SELECT * FROM memory_state WHERE theme = '{selected_theme}'"
+        select_exercise_query = (
+            f"SELECT * FROM memory_state WHERE theme = '{selected_theme}'"
+        )
     else:
         select_exercise_query = "SELECT * FROM memory_state"
 
@@ -60,15 +62,22 @@ with tab1:
         answer = f.read()
     if query == answer:
         st.write("Yes, that's it!")
-    for n_days in [2, 7, 21]:
-        if st.button(f"Review in {n_days} days"):
-            next_review = date.today() + timedelta(days=n_days)
-            con.execute(f"UPDATE memory_state SET last_reviewed = '{next_review}' WHERE exercise_name = '{exercise_name_selected}'")
-            st.rerun()
+
+    cols = st.columns(3)
+
+    for col, n_days in zip(cols, [2, 7, 21]):
+        with col:
+            if st.button(f"Review in {n_days} days"):
+                next_review = date.today() + timedelta(days=n_days)
+                con.execute(
+                    f"UPDATE memory_state SET last_reviewed = '{next_review}' \
+                    WHERE exercise_name = '{exercise_name_selected}'",
+                )
+                st.rerun()
 
 with tab2:
     exercise_instructions = exercise_selected.loc[0, "instructions"]
-    with open(f"instructions/{exercise_instructions}", "r") as f:
+    with open(f"instructions/{exercise_instructions}", "r", encoding="utf-8") as f:
         instructions = f.read()
     st.text(instructions)
     exercise_tables = exercise_selected.loc[0, "tables"]
