0e5cd4734f22f338d9615c942b3c631e9529b80a
Yooby Gigandet <yooby@macbook-pro-de-yooby.home>
2025-12-06 11:42:01 +0100

    Remove the SQL part in if/else

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ Application for reviewing programming languages
 """
 )
 
-con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
-
 with st.sidebar:
     available_theme_df = con.execute("SELECT * FROM memory_state").df()
     available_theme = available_theme_df[
@@ -38,61 +36,78 @@ with st.sidebar:
         index=None,
         placeholder="Select theme",
     )
-    if selected_theme:
-        select_exercise_query = (
-            f"SELECT * FROM memory_state WHERE theme = '{selected_theme}'"
+    if selected_language == "SQL":
+        con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
+        available_theme_df = con.execute("SELECT * FROM memory_state").df()
+        available_theme = available_theme_df[
+            pd.to_datetime(available_theme_df["last_reviewed"]).dt.date <= date.today()
+        ]["theme"].unique()
+        selected_theme = st.selectbox(
+            "Select theme:",
+            available_theme,
+            index=None,
+            placeholder="Select theme",
         )
+        if selected_theme:
+            select_exercise_query = (
+                f"SELECT * FROM memory_state WHERE theme = '{selected_theme}'"
+            )
+        else:
+            select_exercise_query = "SELECT * FROM memory_state"
+
+        exercise_selected = (
+            con.execute(select_exercise_query)
+            .df()
+            .sort_values("last_reviewed")
+            .reset_index(drop=True)
+        )
+        exercise_name_selected = exercise_selected.loc[0, "exercise_name"]
     else:
-        select_exercise_query = "SELECT * FROM memory_state"
+        st.write("Nothing yet")
 
-    exercise_selected = (
-        con.execute(select_exercise_query)
-        .df()
-        .sort_values("last_reviewed")
-        .reset_index(drop=True)
-    )
-    exercise_name_selected = exercise_selected.loc[0, "exercise_name"]
+if selected_language == "SQL":
+    tab1, tab2, tab3, tab4 = st.tabs(["Exercise", "Tables", "Expected result", "Solution"])
 
-tab1, tab2, tab3, tab4 = st.tabs(["Exercise", "Tables", "Expected result", "Solution"])
+    with tab1:
+        query = st.text_area("Write your query here")
+        if query:
+            result_user = con.execute(query).df()
+            st.dataframe(result_user)
+        exercise_answer = exercise_selected.loc[0, "answer"]
+        with open(f"answers/{exercise_answer}", "r", encoding="utf-8") as f:
+            answer = f.read()
+        if query == answer:
+            st.write("Yes, that's it!")
+            st.balloons()
 
-with tab1:
-    query = st.text_area("Write your query here")
-    if query:
-        result_user = con.execute(query).df()
-        st.dataframe(result_user)
-    exercise_answer = exercise_selected.loc[0, "answer"]
-    with open(f"answers/{exercise_answer}", "r", encoding="utf-8") as f:
-        answer = f.read()
-    if query == answer:
-        st.write("Yes, that's it!")
-        st.balloons()
+        cols = st.columns(3)
 
-    cols = st.columns(3)
+        for col, n_days in zip(cols, [2, 7, 21]):
+            with col:
+                if st.button(f"Review in {n_days} days"):
+                    next_review = date.today() + timedelta(days=n_days)
+                    con.execute(
+                        f"UPDATE memory_state SET last_reviewed = '{next_review}' \
+                        WHERE exercise_name = '{exercise_name_selected}'",
+                    )
+                    st.rerun()
 
-    for col, n_days in zip(cols, [2, 7, 21]):
-        with col:
-            if st.button(f"Review in {n_days} days"):
-                next_review = date.today() + timedelta(days=n_days)
-                con.execute(
-                    f"UPDATE memory_state SET last_reviewed = '{next_review}' \
-                    WHERE exercise_name = '{exercise_name_selected}'",
-                )
-                st.rerun()
+    with tab2:
+        exercise_instructions = exercise_selected.loc[0, "instructions"]
+        with open(f"instructions/{exercise_instructions}", "r", encoding="utf-8") as f:
+            instructions = f.read()
+        st.text(instructions)
+        exercise_tables = exercise_selected.loc[0, "tables"]
+        for table in exercise_tables:
+            st.write(f"Table: {table}")
+            df_table = con.execute(f"SELECT * FROM {table}").df()
+            st.dataframe(df_table)
 
-with tab2:
-    exercise_instructions = exercise_selected.loc[0, "instructions"]
-    with open(f"instructions/{exercise_instructions}", "r", encoding="utf-8") as f:
-        instructions = f.read()
-    st.text(instructions)
-    exercise_tables = exercise_selected.loc[0, "tables"]
-    for table in exercise_tables:
-        st.write(f"Table: {table}")
-        df_table = con.execute(f"SELECT * FROM {table}").df()
-        st.dataframe(df_table)
+    with tab3:
+        exercise_answer_query = con.execute(answer)
+        st.dataframe(exercise_answer_query)
 
-with tab3:
-    exercise_answer_query = con.execute(answer)
-    st.dataframe(exercise_answer_query)
-
-with tab4:
-    st.text(answer)
+    with tab4:
+        st.text(answer)
+else:
+    st.write("On progress")
